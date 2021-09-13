9195b190fbbaaa2a78b11696db921fd1b84ec704
João Carlos <joaocarloslima@me.com>
2021-09-13 10:21:17 -0300

    :mag_right: movie search

--- src/main/java/br/com/fiap/movies/controller/MovieController.java
+++ src/main/java/br/com/fiap/movies/controller/MovieController.java
@@ -6,22 +6,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.fiap.movies.model.Movie;
 import br.com.fiap.movies.repository.MovieRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class MovieController {
 	
 	@Autowired
 	private MovieRepository repository;
 	
 	@RequestMapping("/")
-	public ModelAndView index() {
+	public ModelAndView index(@RequestParam(required = false) String s) {
 		ModelAndView modelAndView = new ModelAndView("index");
-		modelAndView.addObject("movies", repository.findAll());
+		if (s == null) {
+			modelAndView.addObject("movies", repository.findAll());
+			log.info("busca completa");
+		}else {
+			modelAndView.addObject("movies", repository.findByDescriptionContaining(s));
+			log.info("busca por " + s);
+		}
+		
 		return  modelAndView;
 	}
 	
@@ -33,6 +43,7 @@ public class MovieController {
 	@PostMapping("/create")
 	public String save(Movie movie, RedirectAttributes redirect) {
 		repository.save(movie);
+		log.info("insert de filme " + movie);
 		redirect.addFlashAttribute("message", "filme cadastrado com sucesso");
 		return  "redirect:/";
 	}
@@ -45,6 +56,7 @@ public class MovieController {
 	@GetMapping("/delete/{id}")
 	public String about(@PathVariable Long id, RedirectAttributes redirect) {
 		redirect.addFlashAttribute("message", "filme apagado com sucesso");
+		log.info("delete de filme id=" + id);
 		repository.deleteById(id);
 		return  "redirect:/";
 	}
--- src/main/java/br/com/fiap/movies/repository/MovieRepository.java
+++ src/main/java/br/com/fiap/movies/repository/MovieRepository.java
@@ -1,9 +1,13 @@
 package br.com.fiap.movies.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fiap.movies.model.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long>{
+
+	List<Movie> findByDescriptionContaining(String s);
 
 }
